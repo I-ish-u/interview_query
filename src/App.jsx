3581eec6c8671cdafdd25a1ff026df052bbd83e3
anmol gupta <rk521528@gmail.com>
2025-01-13 17:29:04 +0530

    implemented data grid api fetching

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+
+
+export default function App() {
+  const [row, setrow] = useState([]);
+  React.useEffect(() => {
+    const fetchdata = async () => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+      const data = await response.json();
+      const transformeddata = data.map((e) => ({
+        id: e.id,
+        title: e.title,
+        completed: e.completed
+      }));
+      setrow(transformeddata);
+    }
+    fetchdata();
+  }, [])
+
+  const columns = [
+    { field: 'id', headerName: 'Id', width: 150 },
+    { field: 'title', headerName: 'Title', width: 150 },
+    { field: 'completed', headerName: 'completed', width: 150 },
+  ];
+
+  return (
+    <div style={{ height: 500, width: '100%' }}>
+      <DataGrid rows={row} columns={columns} />
+    </div>
+  );
+}
